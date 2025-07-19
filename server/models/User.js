8c105ee2934079c72f553174154b5b4f94323f83
magentaong <139094842+magentaong@users.noreply.github.com>
2025-07-20 02:05:37 +0800

    feat: included name, language and profileImg cause i forgot about it T^T

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,30 @@
 const mongoose = require("mongoose");
+const { nanoid } = require("nanoid");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true }, // or phoneNumber if using phone
-  threadId: { type: String },
-  lastInteraction: { type: Date },
+  cID: { type: String, unique: true }, // unique caregiver ID
+  email: { type: String, required: true, unique: true, trim: true},
+  password: { type: String, required: true, trim: true},
+  name: {type: String, required: true,  trim: true},
+  profileImg: {type: String},
+  language: {type: String},
+}, { timestamps: true });
+
+// Auto-generate cID
+userSchema.pre("save", function (next) {
+  if (!this.cID) {
+    this.cID = `caregiver_${nanoid(8)}`;
+  }
+  next();
+});
+
+// Hash password because we not naughty ppl
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
