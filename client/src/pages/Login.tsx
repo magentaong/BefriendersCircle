5c714b9d7b2cd62fdefc9a4c18d22e33b2d8d42e
magentaong <139094842+magentaong@users.noreply.github.com>
2025-07-20 02:04:09 +0800

    feat: added login, onboarding, and profile pages w/ basic functionalities

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import { login, register } from "../api/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignup, setIsSignup] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = isSignup
+        ? await register(email, password, name)
+        : await login(email, password);
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("cID", data.cID);
+
+      if (data.isOnboarded) {
+        navigate("/");
+      } else {
+        navigate("/onboarding");
+      }
+    } catch (err: any) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  return (
+    <main className="min-h-screen flex flex-col items-center px-6 py-30 bg-white text-center">
+      <img src="/ESC.svg" alt="Logo" className="w-15 mb-6" />
+    {/* Back arrow */}
+    <div className="absolute top-10 left-6">
+        <Link to="/">
+        <button className="w-12 h-12 rounded-full bg-latte shadow text-lg flex items-center justify-center">
+            <ArrowLeft className="w-6 h-6" />
+        </button>
+        </Link>
+    </div>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-md bg-white space-y-6 py-5 "
+      >
+
+        {/* Title */}
+        <h2 className="text-xl font-semibold text-gray-800 mb-2 py-2">
+          {isSignup
+            ? "Register with us to join the community!"
+            : "Want to access our caregiver community?"}
+        </h2>
+
+        {/* Name Input (Signup only) */}
+        {isSignup && (
+          <input
+            className="w-full p-3 border border-yellow-300 rounded-xl focus:outline-none placeholder:text-gray-400"
+            placeholder="Username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        )}
+
+        {/* Email */}
+        <input
+          className="w-full p-3 border border-yellow-300 rounded-xl focus:outline-none placeholder:text-gray-400 mt-4"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          required
+        />
+
+        {/* Password */}
+        <input
+          className="w-full p-3 border border-yellow-300 rounded-xl focus:outline-none placeholder:text-gray-400"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          required
+        />
+
+        {/* Error */}
+        {errorMessage && (
+          <div className="text-red-500 text-sm">{errorMessage}</div>
+        )}
+
+        {/* Toggle Signup/Login */}
+        <button
+          type="button"
+          className="text-sm text-[#964B00] underline hover:opacity-80"
+          onClick={() => setIsSignup(!isSignup)}
+        >
+          {isSignup ? "Already have an account? Log in" : "New here? Sign up"}
+        </button>
+        
+        {/* Buttons */}
+        <div className="flex gap-4 justify-center">
+          {!isSignup && (
+            <button
+              type="submit"
+              className="bg-[#D9CBC2] text-black px-6 py-2 rounded-2xl shadow hover:brightness-95"
+            >
+              Log In
+            </button>
+          )}
+          {isSignup && (
+            <button
+                type="submit"
+                className="bg-latte text-black px-6 py-2 rounded-2xl shadow hover:brightness-95"
+            >
+                Register
+            </button>
+            )}
+
+        </div>
+      </form>
+    </main>
+  );
+}
---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,101 @@
+import { Link, useNavigate } from "react-router-dom";
+import { LogOut, Trash2, Pencil } from "lucide-react";
+import { fetchCurrentUser } from "../api/auth";
+import { useEffect, useState } from "react";
+
+export default function Profile() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [language, setLanguage] = useState("English");
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("cID");
+    navigate("/login");
+  };
+
+  const handleDelete = () => {
+    // add your API call here for delete account
+    alert("Account deleted forever (not really yet HEUHEUUE)");
+  };
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const user = await fetchCurrentUser();
+        setName(user.name);
+        setLanguage(user.language);
+      } catch (err) {
+        console.error("Failed to load user", err);
+      }
+    };
+    loadUser();
+  }, []);
+
+  return (
+    <main className="w-full mx-auto px-5 py-5  max-w-md">
+      {/* Header */}
+      <header className="w-full flex items-center justify-between mb-8">
+        <div className="flex items-center gap-2">
+          <Link to="/">
+            <img src="/ESC.svg" alt="BefriendersCircle Logo" className="h-15 w-15" />
+           </Link>  
+          <h1 className="text-xl font-bold text-gray-700">Profile</h1>
+        </div>
+
+        {/* see how we wanna deal with nav :O 
+        <div className="flex gap-3">
+          <Link to="/forum">
+            <button className="bg-blossom px-4 py-1 rounded-full text-charcoal shadow">Network</button>
+          </Link>
+          <Link to="/resources"> 
+            <button className="bg-pistachio px-4 py-1 rounded-full text-charcoal shadow">Resource</button>
+          </Link>
+          <Link to="/training"> 
+            <button className="bg-serene px-4 py-1 rounded-full text-charcoal shadow">Training</button>
+          </Link>
+          <img src="/Avatar.png" alt="Avatar" className="w-10 h-10 rounded-full border border-gray-300"/>
+        </div>
+        */}
+      </header>
+
+      {/* Profile Card */}
+      <div className="flex justify-center"> 
+        <section className="bg-canary p-6 rounded-3xl shadow-md w-full relative text-center max-w-md">
+            <h2 className="text-lg font-semibold text-gray-700 mb-3">Caregiver</h2>
+
+            <button className="absolute top-4 right-4 bg-white rounded-full p-1 shadow">
+                <Pencil className="h-6 w-6 text-gray-600 p-1" />
+            </button>
+
+            <img src="/Avatar.png" alt="Profile Avatar" className="w-30 h-30 mx-auto mb-4"/>
+
+            <p className="text-gray-700 font-medium">Name : {name || "No Name"} </p>
+            {/* <p className="text-gray-700 font-medium">Password : f********</p> 
+                boss i encrypted the password so uhm oops            
+            */}
+            <p className="text-gray-700 font-medium">Language : {language || "English"}</p>
+
+            {/* Buttons */}
+            <div className="mt-6 flex flex-col sm:flex-row gap-4 justify-center">
+                <button
+                    onClick={handleLogout}
+                    className="flex items-center justify-center gap-2 px-6 py-3 bg-latte text-black rounded-2xl shadow-md hover:brightness-95 w-full sm:w-auto">
+                    <LogOut className="w-5 h-5" />
+                    <span className="font-semibold">Log Out</span>
+                </button>
+
+                <button
+                    onClick={handleDelete}
+                    className="flex items-center justify-center gap-2 px-6 py-3 bg-[#E78686] text-black rounded-2xl shadow-md hover:brightness-95 w-full sm:w-auto">
+                    <Trash2 className="w-5 h-5" />
+                    <span className="font-semibold">Delete Forever</span>
+                </button>
+            </div>
+
+        </section>
+      </div>
+    </main>
+  );
+}
+
+//TODO: Edit modal for profile :(
